80c562fd2c4696790c3deefca962bdf1a2568611
Samson Ajulor <samsonajulor@gmail.com>
2023-09-11 07:13:03 +0100

    moded swap interaction script

--- README.md
+++ README.md
@@ -9,3 +9,16 @@ This Contract was deployed to sepolia testnet via the following commands
 - RUN `npx hardhat run scripts/deployUSDCTok.ts --network sepolia`
 - copy the deployed contract address and save the input into the USDCTok variable of the swapper.sol constructor.
 - RUN `npx hardhat run scripts/deploySwapper.ts --network sepolia`
+
+#### Verifying the tokens
+- RUN `npx hardhat verify --contract contracts/ethTok.sol:ETHToken  --network sepolia 0x18aBf89D5932B62Ee68101D9F689c4200FA55096` to verify the ethTok
+- Verification url: [ethToken url](https://sepolia.etherscan.io/address/0x18aBf89D5932B62Ee68101D9F689c4200FA55096#code)
+- RUN `npx hardhat verify --contract contracts/usdcTok.sol:USDCToken  --network sepolia 0x57Df052B630Dd1a713C91CB87FAE9fC03AA3182a` to verify the usdcTok
+- Verification url: [usdcToken url](https://sepolia.etherscan.io/address/0x57Df052B630Dd1a713C91CB87FAE9fC03AA3182a#code)
+
+
+#### Running a local node of the forked sepolia network
+- RUN `npx hardhat node`
+
+#### Interacting with the contract locally
+- RUN `npx hardhat run scripts/swapperInteract.ts --network localhost`
--- src/scripts/swapperInteract.ts
+++ src/scripts/swapperInteract.ts
@@ -1,51 +1,62 @@
 import { ethers } from 'hardhat';
+import {ITOK} from '../typechain-types/contracts/interfaces/ITOK';
 
 // token address at sepolia = 0x0dF9E9f0FC5aDbd840e7332DDE021c6B3C1e5e7C
+
 async function main() {
+  // get signers
+  const [deployer] = await ethers.getSigners();
   const swapperAddress = '0x0dF9E9f0FC5aDbd840e7332DDE021c6B3C1e5e7C';
+  const USDCToken = '0x57Df052B630Dd1a713C91CB87FAE9fC03AA3182a';
+  const ETHToken = '0x57Df052B630Dd1a713C91CB87FAE9fC03AA3182a';
+  const uniswapProtoContract = await ethers.getContractAt('ISwapper', swapperAddress);
+  const usdcTokenContract = await ethers.getContractAt('ITOK', USDCToken);
+  const ethTokenContract = await ethers.getContractAt('ITOK', ETHToken);
 
-  const uniswapContract = await ethers.getContractAt('ISwapper', swapperAddress);
-
-  // set signer's balance
-  // await network.provider.send('hardhat_setBalance', [UNIHOLDER, '0x9DC0ED08D31E336800']);
-
-  const ethBalanceBefore = await uniswapContract.getETHTokReserves();
+  const ethBalanceBefore = await uniswapProtoContract.getETHTokReserves();
   console.log('ethBalanceBefore', ethBalanceBefore.toString());
-  const usdcBalanceBefore = await uniswapContract.getUSDCTokReserves();
+  const usdcBalanceBefore = await uniswapProtoContract.getUSDCTokReserves();
   console.log('usdcBalanceBefore', usdcBalanceBefore.toString());
 
   // add liquidity
-  await uniswapContract.AddLiquidity(200, 200);
+  await uniswapProtoContract.AddLiquidity(200, 200);
   // log the reserves
-  const ethBalanceAfter = await uniswapContract.getETHTokReserves();
+  const ethBalanceAfter = await uniswapProtoContract.getETHTokReserves();
   console.log('ethBalanceAfter', ethBalanceAfter.toString());
-  const usdcBalanceAfter = await uniswapContract.getUSDCTokReserves();
+  const usdcBalanceAfter = await uniswapProtoContract.getUSDCTokReserves();
   console.log('usdcBalanceAfter', usdcBalanceAfter.toString());
 
   // remove liquidity
-  await uniswapContract.removeLiquidity(200, 200);
+  await uniswapProtoContract.removeLiquidity(200, 200);
   // log the reserves
-  const ethBalanceAfter2 = await uniswapContract.getETHTokReserves();
+  const ethBalanceAfter2 = await uniswapProtoContract.getETHTokReserves();
   console.log('ethBalanceAfterRemoval', ethBalanceAfter2.toString());
-  const usdcBalanceAfter2 = await uniswapContract.getUSDCTokReserves();
+  const usdcBalanceAfter2 = await uniswapProtoContract.getUSDCTokReserves();
   console.log('usdcBalanceAfterRemoval', usdcBalanceAfter2.toString());
 
+  // allow usdc to be spent by swapper
+  await usdcTokenContract.allowance(deployer.address, swapperAddress);
+  // approve usdc
+  await usdcTokenContract.approve(swapperAddress, 1000000000000);
   // swap eth for usdc
-  await uniswapContract.swapETHTokForUSDCTok(100);
+  await uniswapProtoContract.swapETHTokForUSDCTok(100);
   // log the reserves
-  const ethBalanceAfter3 = await uniswapContract.getETHTokReserves();
+  const ethBalanceAfter3 = await uniswapProtoContract.getETHTokReserves();
   console.log('ethBalanceAfterSwap', ethBalanceAfter3.toString());
-  const usdcBalanceAfter3 = await uniswapContract.getUSDCTokReserves();
+  const usdcBalanceAfter3 = await uniswapProtoContract.getUSDCTokReserves();
   console.log('usdcBalanceAfterSwap', usdcBalanceAfter3.toString());
 
+  // allow eth to be spent by swapper
+  await ethTokenContract.allowance(deployer.address, swapperAddress);
+  // approve eth
+  await ethTokenContract.approve(swapperAddress, 1000000000000);
   // swap usdc for eth
-  await uniswapContract.swapUSDCTokForETHTok(100);
+  await uniswapProtoContract.swapUSDCTokForETHTok(100);
   // log the reserves
-  const ethBalanceAfter4 = await uniswapContract.getETHTokReserves();
+  const ethBalanceAfter4 = await uniswapProtoContract.getETHTokReserves();
   console.log('ethBalanceAfterSwap', ethBalanceAfter4.toString());
-  const usdcBalanceAfter4 = await uniswapContract.getUSDCTokReserves();
+  const usdcBalanceAfter4 = await uniswapProtoContract.getUSDCTokReserves();
   console.log('usdcBalanceAfterSwap', usdcBalanceAfter4.toString());
-
 }
 
 main().catch((error) => {
